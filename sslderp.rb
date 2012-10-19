8856f9f31d7c685e9ed8f63461c82d966d41ca6c
James Sheridan <james@yammer-inc.com>
2012-10-19 19:07:30 +0100

    this is totally broken right now

--- sslderp.rb
+++ sslderp.rb
@@ -2,24 +2,48 @@
 
 require 'net/https'
 require 'date'
+require 'optparse'
 
-unless ARGV.length > 0
-    puts "No target specified. DERP."
-    puts "Usage: sslderp <hostname>"
-    exit
+options = {}
+
+opt_parser = OptionParser.new do |opt|
+    opt.banner = "Usage: sslderp -t <hostname> [OPTIONS]"
+    options[:verbose] = false
+
+    opt.on("-v", "--verbose", "Be verbose") do
+        options[:verbose] = true
+    end
+    
+    opt.on("-t", "--target HOSTNAME", "Target SSL domain to check") do |target|
+        options[:target] = target
+    end
+    opt.on("-n", "--nagios", "Produce nagios compatible output") do
+        options[:nagios] = true
+    end
+
+    opt.on("-h", "--help", "Some useful help, derps.") do
+        puts opt
+        exit
+    end
 end
 
+opt_parser.parse!
 
-uri = URI.parse("https://#{ARGV}")
-http = Net::HTTP::new(uri.host,uri.port)
-http.use_ssl = true
-http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-http.start do |h|
-  @cert = h.peer_cert
+if options[:target]
+   uri = URI.parse("https://#{options[:target]}")
+    
+   http = Net::HTTP::new(uri.host,uri.port)
+   http.use_ssl = true
+   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+   http.start do |h|
+       @cert = h.peer_cert
+   end
+
+   today = Date.today
+
+   expiry = Date.parse("#{@cert.not_after}")
+   days = expiry - today
+   puts days
+else
+    puts opt
 end
-
-today = Date.today
-
-expiry = Date.parse("#{@cert.not_after}")
-days = expiry - today
-puts days
