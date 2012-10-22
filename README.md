2822e0ba660de7a92ed6c035ffabc1b3fd54f3ab
James Sheridan <james@yammer-inc.com>
2012-10-22 14:18:36 +0100

    Added Nagios-compatible exit codes and some more graceful error handling

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 sslderp
 
-Simple utility to report things about remotely hosted SSL certs.
+This is a simple utility to report things about remotely hosted SSL certs.
 
+Presently, it will make a HTTPS connection to a given server and will report
+the number of days remaning until that certificate expires. 
+
+Example usage:
+sslderp --target www.example.org --nagios
+
+If you pass -n or --nagios to the utility, it will return Nagios/Icinga-
+compatible exit codes if the certificate is close to expiry. <30 days will
+generate a WARNING (exit 1), <15 days will generate a CRITICAL (exit 2) and
+anything >30 days will return a clean 0.
 
--- sslderp.rb
+++ sslderp.rb
@@ -9,6 +9,7 @@ options = {}
 opt_parser = OptionParser.new do |opt|
     opt.banner = "Usage: sslderp -t <hostname> [OPTIONS]"
     options[:verbose] = false
+    options[:nagios] = false
 
     opt.on("-v", "--verbose", "Be verbose") do
         options[:verbose] = true
@@ -27,23 +28,58 @@ opt_parser = OptionParser.new do |opt|
     end
 end
 
-opt_parser.parse!
+begin
+    opt_parser.parse!
+    mandatory = [:target]
+    missing = mandatory.select{ |param| options[param].nil? }
+    if not missing.empty?
+        puts "Specifying a target is necessary. #{missing.join(', ')}"
+        puts opt_parser
+        exit
+    end
 
-if options[:target]
-   uri = URI.parse("https://#{options[:target]}")
-    
-   http = Net::HTTP::new(uri.host,uri.port)
-   http.use_ssl = true
-   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-   http.start do |h|
-       @cert = h.peer_cert
-   end
-
-   today = Date.today
-
-   expiry = Date.parse("#{@cert.not_after}")
-   days = expiry - today
-   puts days
-else
-    puts opt
+    rescue OptionParser::InvalidOption, OptionParser::MissingArgument
+        puts $!.to_s
+        puts opt_parser
+        exit
 end
+
+begin
+    uri = URI.parse("https://#{options[:target]}")
+    
+    http = Net::HTTP::new(uri.host,uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    http.start do |h|
+        @cert = h.peer_cert
+    end
+
+    rescue SocketError
+        puts $!.to_s
+        exit
+
+end
+
+today = Date.today
+expiry = Date.parse("#{@cert.not_after}")
+days = expiry - today
+days = days.truncate
+
+if options[:nagios] == true
+    if days < 30
+        if days < 15
+            puts days
+            exit 2
+        end
+        puts days
+        exit 1
+    else
+        puts days
+        exit 0
+    end
+
+else
+    puts days
+end
+
